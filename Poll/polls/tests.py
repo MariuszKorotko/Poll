81b364ed87bf06c05d39cbaa6b4af661597f4ca4
Mariusz Korotko <mariusz.korotko@wp.pl>
2017-07-27 18:44:01 +0200

    Create method 'create_qustion' in tests.py

--- Poll/polls/tests.py
+++ Poll/polls/tests.py
@@ -2,6 +2,15 @@ import datetime
 from django.utils import timezone
 from django.test import TestCase
 from .models import Question
+from django.urls
+
+def create_question(question_text, days):
+    """Create a question with given 'question_text' and published the given
+    number of 'days' offset to now (negative for questions published in the
+    past, positive for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
 
 class QuestionTest(TestCase):
     def test_was_publised_recently_with_future_question(self):
