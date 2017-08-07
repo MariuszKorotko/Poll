68080509879a2046bd0208ab54eda865799cbc63
Mariusz Korotko <mariusz.korotko@wp.pl>
2017-08-07 17:13:34 +0200

    Test future and past question

--- Poll/polls/tests.py
+++ Poll/polls/tests.py
@@ -48,6 +48,19 @@ class QuestionTest(TestCase):
         self.assertContains(response, "No polls are available.")
         self.assertQuerysetEqual(response.context['latest_question_list'], [])
 
+    def test_future_question_and_past_question(self):
+        """
+        Even if both past questions and future question exist, only past
+        questions are displayed.
+        """
+        create_question(question_text="Past question.", days=-30)
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'], ['<Question: Past '
+                                                       'question.>']
+        )
+
     def test_was_publised_recently_with_future_question(self):
         """
         was_published_recently() returns False for questions whose pub_date
