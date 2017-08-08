5918f1a660bfca17ad249b24d7980c2692ca7d43
Mariusz Korotko <mariusz.korotko@wp.pl>
2017-08-08 17:34:32 +0200

    Refacotr code

--- Poll/polls/tests.py
+++ Poll/polls/tests.py
@@ -6,7 +6,6 @@ from django.utils import timezone
 
 from .models import Question
 
-
 def create_question(question_text, days):
     """
     Create a question with given 'question_text' and published the given
@@ -15,7 +14,6 @@ def create_question(question_text, days):
     """
     time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date=time)
-
 
 class QuestionTest(TestCase):
     def no_question(self):
--- Poll/polls/views.py
+++ Poll/polls/views.py
@@ -17,11 +17,9 @@ class IndexView(generic.ListView):
         return Question.objects.filter(pub_date__lte=timezone.now(
         )).order_by('-pub_date')[:5]
 
-
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
-
 
 class ResultsView(generic.DetailView):
     model = Question
