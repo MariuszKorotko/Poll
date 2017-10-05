30f2769583cf5adef496a149d34a7230f52ea553
Mariusz Korotko <mariusz.korotko@wp.pl>
2017-10-05 10:31:43 +0200

    urls.py add login url

--- Poll/polls/urls.py
+++ Poll/polls/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
-from . import views
+from django.contrib.auth import views as auth_views
+from .views import DetailView, ResultsView, vote
 
 app_name = 'polls'
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^$', auth_views.login,
+        {'template_name': 'polls/index.html'},
+    name='index-login'),
+    url(r'^(?P<pk>[0-9]+)/$', DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', ResultsView.as_view(), name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', vote, name='vote'),
 ]
