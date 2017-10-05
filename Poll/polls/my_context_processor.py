99ca9447cfe5eec3f03519b7aab7c5916bcb7a60
Mariusz Korotko <mariusz.korotko@wp.pl>
2017-10-05 11:11:40 +0200

    Recovery app

--- Poll/polls/my_context_processor.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-def footer_cp(request):
-    """Display data time and 'based on'"""
-    context = {
-                "now": datetime.datetime.now(),
-                "based_on" : "Based on the Django tutorial."
-              }
-    return context
